3d141b4e83d461c8bd87edc698e7e37d4e9ba0d3
ahmaddimas <ahmad_muttaqi_24rpl@student.smktelkom-mlg.sch.id>
2017-02-03 00:13:07 +0700

    asp.net web login and signup with adminonly page

---
+++ asp_mvc_localDatabase/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System.Web.Mvc;
+using asp_mvc_localDatabase.Security;
+
+namespace asp_mvc_localDatabase.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult Welcome()
+        {
+            return View();
+        }
+
+        [AuthorizeRoles("Admin")]
+        public ActionResult AdminOnly()
+        {
+            return View();
+        }
+
+        public ActionResult UnAuthorized()
+        {
+            return View();
+        }
+    }
+}
---
+++ asp_mvc_localDatabase/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="asp_mvc_localDatabase.MvcApplication" Language="C#" %>
